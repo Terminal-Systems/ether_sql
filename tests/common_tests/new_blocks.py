c632cb36eece6e9d22dbe71bb35525c53c28d074
jsonsivar <jsonsivar@gmail.com>
2018-12-16 18:24:55 -0500

    Update tests to factor in network

--- tests/common_tests/new_blocks.py
+++ tests/common_tests/new_blocks.py
@@ -54,7 +54,8 @@ def put_initial_blocks_in_waiting():
                               block_hash=to_hex(block_hash))
     verify_pushed_sql_contents()
     with current_session.db_session_scope():
-        block_task_meta = current_session.db_session.query(BlockTaskMeta).first()
+        block_task_meta = current_session.db_session.query(BlockTaskMeta).\
+        filter_by(network=current_session.network).first()
         assert block_task_meta.block_number == BASE_BLOCK
         assert block_task_meta.block_hash == EXPECTED_BLOCK_HASHES[BASE_BLOCK]
         assert block_task_meta.state == 'WAITING'
@@ -64,7 +65,8 @@ def push_initial_blocks_in_queue():
     current_session = get_current_session()
     push_blocks_in_queue()
     with current_session.db_session_scope():
-        block_task_meta = current_session.db_session.query(BlockTaskMeta).first()
+        block_task_meta = current_session.db_session.query(BlockTaskMeta).\
+        filter_by(network=current_session.network).first()
         assert block_task_meta.block_number == BASE_BLOCK
         assert block_task_meta.block_hash == EXPECTED_BLOCK_HASHES[BASE_BLOCK]
         assert block_task_meta.state == 'PENDING'
@@ -72,7 +74,8 @@ def push_initial_blocks_in_queue():
 def verify_pushed_block_56160_contents():
     current_session = get_current_session()
     with current_session.db_session_scope():
-        block_task_meta = current_session.db_session.query(BlockTaskMeta).first()
+        block_task_meta = current_session.db_session.query(BlockTaskMeta).\
+        filter_by(network=current_session.network).first()
         task_id = block_task_meta.task_id
     task_completed = AsyncResult(task_id).get()
     verify_block_56160_contents()
@@ -80,18 +83,27 @@ def verify_pushed_block_56160_contents():
 def verify_pushed_sql_contents():
     current_session = get_current_session()
     with current_session.db_session_scope():
-        assert current_session.db_session.query(Blocks).count() == 0
-        assert current_session.db_session.query(Transactions).count() == 0
-        assert current_session.db_session.query(Receipts).count() == 0
-        assert current_session.db_session.query(Logs).count() == 0
-        assert current_session.db_session.query(Uncles).count() == 0
+        assert current_session.db_session.query(Blocks).\
+        filter_by(network=current_session.network).count() == 0
+        assert current_session.db_session.query(Transactions).\
+        filter_by(network=current_session.network).count() == 0
+        assert current_session.db_session.query(Receipts).\
+        filter_by(network=current_session.network).count() == 0
+        assert current_session.db_session.query(Logs).\
+        filter_by(network=current_session.network).count() == 0
+        assert current_session.db_session.query(Uncles).\
+        filter_by(network=current_session.network).count() == 0
 
         if current_session.settings.PARSE_TRACE:
-            assert current_session.db_session.query(Traces).count() == 0
+            assert current_session.db_session.query(Traces).\
+            filter_by(network=current_session.network).count() == 0
 
         if current_session.settings.PARSE_STATE_DIFF:
-            assert current_session.db_session.query(StateDiff).count() == 0
-            assert current_session.db_session.query(StorageDiff).count() == 0
+            assert current_session.db_session.query(StateDiff).\
+            filter_by(network=current_session.network).count() == 0
+            assert current_session.db_session.query(StorageDiff).\
+            filter_by(network=current_session.network).count() == 0
 
-        assert current_session.db_session.query(BlockTaskMeta).count() == \
+        assert current_session.db_session.query(BlockTaskMeta).\
+        filter_by(network=current_session.network).count() == \
             current_session.settings.BLOCK_LAG
